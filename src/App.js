75f9b0307896ae275a19f494c7067235df6bffd2
Mark Paget <marcuspaget@gmail.com>
2019-05-23 22:38:54 +0800

    changed app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-		Gab buttie lush
+		I was changed on feature branch
 
         </p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer" >
